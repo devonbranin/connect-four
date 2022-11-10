3ce072b488770c1b0747f46b70d8f6ec856ffe09
Devon Branin <devonkbranin@gmail.com>
2022-11-09 20:21:35 -0800

    Add partial diagonal logic

--- lib/player.rb
+++ lib/player.rb
@@ -24,36 +24,50 @@ class Player
   def win?(array = move_array)
     return true if horizontal_win?(array) == true
     return true if vertical_win?(array) == true
+    return true if diagonal_win?(array) == true
+    return true if other_diagonal_win?(array) == true
 
     false
   end
 
   def horizontal_win?(array = move_array)
+    consecutive = ->(a, b) { a + b == (a * 2) + 1 }
+    same_row = ->(a, b) { (a / COLUMNS) == (b / COLUMNS) }
     four_piece_combos = all_fours(array)
-    horiz_win = four_piece_combos.filter { |combo| all_consecutive?(combo) }
-    horiz_win.any? { |combo| all_same_row?(combo) }
+    horiz_win = four_piece_combos.filter { |combo| validate_combinations(combo, consecutive) }
+    horiz_win.any? { |combo| validate_combinations(combo, same_row) }
   end
 
   def vertical_win?(array = move_array)
+    consecutive_row = ->(a, b) { row_number(a) + row_number(b) == (row_number(a) * 2) + 1 }
+    same_column = ->(a, b) { (a % COLUMNS) == (b % COLUMNS) }
     four_piece_combos = all_fours(array)
-    vert_win = four_piece_combos.filter { |combo| consecutive_rows?(combo) }
-    vert_win.any? { |combo| all_same_column?(combo) }
+    vert_win = four_piece_combos.filter { |combo| validate_combinations(combo, consecutive_row) }
+    vert_win.any? { |combo| validate_combinations(combo, same_column) }
   end
 
-  def same_row?(a, b)
-    (a / COLUMNS) == (b / COLUMNS)
+  def diagonal_win?(array = move_array)
+    consecutive_row = ->(a, b) { row_number(a) + row_number(b) == (row_number(a) * 2) + 1 }
+    consecutive_column = ->(a, b) { column_number(a) + column_number(b) == (column_number(a) * 2) + 1 }
+    four_piece_combos = all_fours(array)
+    diag_win = four_piece_combos.filter { |combo| validate_combinations(combo, consecutive_row) }
+    diag_win.any? { |combo| validate_combinations(combo, consecutive_column) }
   end
 
-  def same_column?(a, b)
-    (a % COLUMNS) == (b % COLUMNS)
+  def other_diagonal_win?(array = move_array)
+    consecutive_row = ->(a, b) { row_number(a) + row_number(b) == (row_number(b) * 2) + 1 }
+    consecutive_column = ->(a, b) { column_number(a) + column_number(b) == (column_number(b) * 2) + 1 }
+    four_piece_combos = all_fours(array)
+    diag_win = four_piece_combos.filter { |combo| validate_combinations(combo, consecutive_row) }
+    diag_win.any? { |combo| validate_combinations(combo, consecutive_column) }
   end
 
   def row_number(number)
     number / COLUMNS
   end
 
-  def consecutive?(a, b)
-    a + b == (a * 2) + 1
+  def column_number(number)
+    number % COLUMNS
   end
 
   def all_fours(array = move_array)
@@ -63,49 +77,10 @@ class Player
     holder_array
   end
 
-  def all_same_row?(my_array)
+  def validate_combinations(my_array, lamb)
     i = 0
     while i + 1 < my_array.length
-      test = same_row?(my_array[i], my_array[i+1])
-      i += 1
-      next if test == false
-    end
-
-    return false if test == false
-
-    true
-  end
-
-  def all_same_column?(my_array)
-    i = 0
-    while i + 1 < my_array.length
-      test = same_column?(my_array[i], my_array[i+1])
-      i += 1
-      next if test == false
-    end
-
-    return false if test == false
-
-    true
-  end
-
-  def all_consecutive?(my_array)
-    i = 0
-    while i + 1 < my_array.length
-      test = consecutive?(my_array[i], my_array[i+1])
-      i += 1
-      next if test == false
-    end
-
-    return false if test == false
-
-    true
-  end
-
-  def consecutive_rows?(my_array)
-    i = 0
-    while i + 1 < my_array.length
-      test = consecutive?(row_number(my_array[i]), row_number(my_array[i+1]))
+      test = lamb.call(my_array[i], my_array[i+1])
       i += 1
       next if test == false
     end
